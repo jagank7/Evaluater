3057e2f87862f8322699e979df7b53a3cbb560bd
SitaRamaiahSingamsetti <ssingams@opentext.com>
2017-03-16 09:12:09 +0530

    Removed unnecessary comments

--- src/main/java/com/example/evaluator/App.java
+++ src/main/java/com/example/evaluator/App.java
@@ -20,49 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class App 
 {
-	/*@RequestMapping("/")
-    String home(@RequestParam("code") String code) {
-		StringBuffer output = new StringBuffer();
-		BufferedWriter writer = null;
-		try {
-			writer = new BufferedWriter(new FileWriter(new java.io.File("C:/Users/user/Solution.java")));
-			writer.write(code);
-			writer.flush();
-			Process p = Runtime.getRuntime().exec("cmd /c javac \"C:/Users/user/Solution.java\"");
-			p.waitFor();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					p.getErrorStream()));
-			String line = reader.readLine();
-			while (line != null) {
-				output.append(line);
-				line = reader.readLine();
-			}
-			
-			if(output.toString().isEmpty()){
-				p = Runtime.getRuntime().exec("cmd /c java -classpath C:/Users/user Solution");
-				p.waitFor();
-				reader = new BufferedReader(new InputStreamReader(
-						p.getInputStream()));
-				String line2 = reader.readLine();
-				while (line2 != null) {
-					output.append(line2).append("<br />");
-					line2 = reader.readLine();
-				}
-			}
-		} catch (IOException e1) {
-			return e1.getMessage();
-		} catch (InterruptedException e2) {
-			return e2.getMessage();
-		}finally{
-			try{
-				if(null != writer)
-					writer.close();
-			}catch(Exception e){
-				return e.getMessage();
-			}
-		}
-        return output.toString();
-    }*/
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
